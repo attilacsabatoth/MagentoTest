2888bad7970ff4538544fe0d05e4c523428afcb3
root <root@debian32.TendaAP>
2012-09-15 17:11:40 +0100

    task two

---
+++ app/code/local/Attila/Tasksecond/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Attila_Tasksecond_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "Today is a happy day";
+    }
+}
+
+?>
